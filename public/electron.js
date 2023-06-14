a8c60b8dacee71670140d72c3d57bf0327966791
JovannMC <45893380+JovannMC@users.noreply.github.com>
2023-06-14 17:40:23 +0300

    Install electron-forge

--- public/electron.js
+++ public/electron.js
@@ -1,56 +1,77 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
-const path = require('path');
-const url = require('url');
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
-
-function createWindow() {
+const createWindow = () => {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            //preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+        }
+    })
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.removeMenu();
+
+    if (app.isPackaged) {
+        mainWindow.loadFile('index.html'); // prod
+    } else {
+        mainWindow.loadURL('http://localhost:3000'); // dev
+    }
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+    mainWindow.webContents.openDevTools()
+        
 }
+
+ipcMain.on('create-window', (event, arg) => {
+        createInstanceWindow();
+});
+
+const createInstanceWindow = () => {
+    // Create the browser window.
+    const ciWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+          //preload: path.join(__dirname, 'preload.js'),
+          nodeIntegration: true,
+      }
+    })
+  
+    ciWindow.removeMenu();
+  
+    if (app.isPackaged) {
+        ciWindow.loadFile('./windows/createInstance.html'); // prod
+    } else {
+        ciWindow.loadURL('http://localhost:3000/windows/createInstance.html'); // dev
+    }
+  
+    // Open the DevTools.
+    ciWindow.webContents.openDevTools()
+    }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow()
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-});
-
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
-});
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
